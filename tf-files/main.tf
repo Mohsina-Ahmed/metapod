3765af7ef0805607d0d11a72df5ebe1a595b25a4
Mohsina-Ahmed <mohsinaahmedchowdhury@gmail.com>
2023-10-04 12:35:20 +0100

    Adding terraform for s3 buckets

---
+++ tf-files/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket = "mohtao"
+    key    = "terraform-state"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  #profile = "terraform"
+  region  = "eu-west-2"
+}
